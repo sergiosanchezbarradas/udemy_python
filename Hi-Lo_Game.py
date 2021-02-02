af3e2d81fe5ac660d0d47de9eafbb01936a57b9c
sergiosanchezbarradas <sergio_sanchez_b@hotmail.com>
2021-02-02 21:44:19 +1100

    e

--- Hi-Lo_Game.py
+++ Hi-Lo_Game.py
@@ -5,8 +5,8 @@ print("Think of a number between {} and {}".format(low, high))
 input("Press ENTER to start the deadly game")
 
 guesses = 1
-while True:
-    print("\tSo, it is between {} and {}".format(low, high))
+while low != high:
+    # print("\tSo, it is between {} and {}".format(low, high))
     guess = low + (high - low) // 2
     high_low = input("My guess is {}. Should I guess higher or lower Sergio master of the Universe? "
                      "Enter h or l,or c if my guess was correct"
@@ -23,4 +23,7 @@ while True:
         print("Enter h, l or c")
 
     guesses += 1
-    # guesses = guesses + 1
+    # guesses = guesses + 1     not the best way
+else:
+    print("Your number is {}".format(low))
+    print("I got it in {} guesses".format(guesses))
---
+++ contrive.py
@@ -0,0 +1,6 @@
+numbers = [1, 45, 31, 12, 60]
+
+for number in numbers:
+    if number % 8 == 0:
+        print("Unacceptable")
+        break
